9e595800063464f5f1c460bffb7baa088981fdf1
Victor Avila <victor.avila@venturplex.com>
2018-05-23 11:00:01 -0700

    edit libs

--- apps/starwars-api/src/app/app.module.ts
+++ apps/starwars-api/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { AppMaterialModule } from '@levelUp/app-material.module';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { SpeciesComponent } from './species/species.component';
 import { HomeComponent } from './home/home.component';
 
