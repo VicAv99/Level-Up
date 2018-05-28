4c2a44c55b683703b21c1d565f9de327ab281451
Victor Avila <victor.avila@venturplex.com>
2018-05-27 21:41:50 -0700

    feature and service

--- apps/poke-api/src/app/app.module.ts
+++ apps/poke-api/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppMaterialModule } from '@levelUp/app-material.module';
 import { HomeComponent } from './home/home.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon/pokemon-detail/pokemon-detail.component';
+import { PokemonService } from './shared/pokemon.service';
 
 @NgModule({
   imports: [
@@ -14,7 +18,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     AppMaterialModule
   ],
-  declarations: [AppComponent, HomeComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, PokemonComponent, PokemonListComponent, PokemonDetailComponent],
+  bootstrap: [AppComponent],
+  providers: [PokemonService]
 })
 export class AppModule {}
