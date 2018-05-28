6aea77365773254a7551b953e00951c8f7c44860
Victor Avila <victor.avila@venturplex.com>
2018-05-27 21:38:20 -0700

    basic poke app structure

--- apps/poke-api/src/app/app.component.ts
+++ apps/poke-api/src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  title = 'Poke API App';
+  links = [
+    { path: '/home', icon: 'home', label: 'Home' },
+    { path: '/<somepath>', icon: 'loyalty', label: '<somelabel>' },
+  ];
   constructor() {}
 
   ngOnInit() {}
--- apps/poke-api/src/app/app.module.ts
+++ apps/poke-api/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from '@levelUp/app-material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    AppRoutingModule,
+    AppMaterialModule
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
