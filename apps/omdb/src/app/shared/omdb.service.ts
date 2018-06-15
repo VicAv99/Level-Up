c06e645663dd6f20c7e01f82eb5fa69b6a3e90cd
Victor Avila <victor.avila@venturplex.com>
2018-06-15 10:31:19 -0700

    edits

--- apps/omdb/src/app/shared/omdb.service.ts
+++ apps/omdb/src/app/shared/omdb.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Movie } from './models/movie';
+import { omdbapi } from 'secret/apis';
 
 const BASE = 'http://www.omdbapi.com/';
 
@@ -14,7 +15,7 @@ export class OmdbService {
     const URL = `${BASE}`;
     let params = new HttpParams();
     params = params.append('t', str)
-    params = params.append('apikey', 'c541c749')
+    params = params.append('apikey', omdbapi)
     return this.http.get<Movie>(URL, {params})
       .pipe(map(res => res));
   }
--- apps/youtube-api/src/app/shared/playlist.service.ts
+++ apps/youtube-api/src/app/shared/playlist.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, tap } from 'rxjs/operators';
-import { api } from "../../api";
+import { ytapi } from 'secret/apis';
 
 const PLAYLIST =
-  `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=PLhv4jOhq_1oXKZZWYlIBWAh2qwNu8hmSl&fields=etag%2Citems(contentDetails(videoId%2CvideoPublishedAt)%2Cetag%2Cid%2Csnippet(channelTitle%2Cdescription%2CpublishedAt%2CresourceId%2FvideoId%2Cthumbnails%2Fhigh%2Furl%2Ctitle))%2Ckind&key=${api}`;
+  `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=PLhv4jOhq_1oXKZZWYlIBWAh2qwNu8hmSl&fields=etag%2Citems(contentDetails(videoId%2CvideoPublishedAt)%2Cetag%2Cid%2Csnippet(channelTitle%2Cdescription%2CpublishedAt%2CresourceId%2FvideoId%2Cthumbnails%2Fhigh%2Furl%2Ctitle))%2Ckind&key=${ytapi}`;
 
 const HERO = 'https://level-json.herokuapp.com/';
 
