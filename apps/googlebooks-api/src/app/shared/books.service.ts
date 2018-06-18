720fd9f7f2b797ca7146a92b529e92cc2515135a
Victor Avila <victor.avila@venturplex.com>
2018-06-18 14:43:09 -0700

    home

--- apps/googlebooks-api/src/app/shared/books.service.ts
+++ apps/googlebooks-api/src/app/shared/books.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+const BASE = 'https://www.googleapis.com/books/v1/volumes?q=search+terms';
 
 @Injectable()
 export class BooksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchBooksApi(str = 'Charity') {
+    let params = new HttpParams();
+    params = params.append('q', str);
+
+    return this.http.get(`${BASE}`, {params})
+      .subscribe(res => res);
+  }
 
 }
