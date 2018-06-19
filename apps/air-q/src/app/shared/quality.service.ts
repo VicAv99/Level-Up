f06d9e7681faed62e3755dff1d9047f206738876
Victor Avila <victor.avila@venturplex.com>
2018-06-19 13:10:03 -0700

    home

--- apps/air-q/src/app/shared/quality.service.ts
+++ apps/air-q/src/app/shared/quality.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+const BASE = 'https://api.openaq.org/v1/locations';
 
 @Injectable()
 export class QualityService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPhoenixLocation(phx = 'Phoenix') {
+    let params = new HttpParams();
+    params = params.append('city', phx)
+
+    return this.http.get(BASE)
+      .pipe(map(res => res));
+  }
 
 }
