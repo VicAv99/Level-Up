479e3fc9183285f63e836a23d15c2c24184b808f
Victor Avila <victor.avila@venturplex.com>
2018-06-19 09:19:08 -0700

    air q generating

--- apps/air-q/src/app/app.module.ts
+++ apps/air-q/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
+import { HomeComponent } from './home/home.component';
+import { LocationsComponent } from './locations/locations.component';
+import { PhoenixComponent } from './locations/phoenix/phoenix.component';
+import { LosAngelesComponent } from './locations/los-angeles/los-angeles.component';
+import { SearchComponent } from './locations/search/search.component';
+import { QualityService } from './shared/quality.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, NxModule.forRoot()],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, NxModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, LocationsComponent, PhoenixComponent, LosAngelesComponent, SearchComponent],
+  bootstrap: [AppComponent],
+  providers: [QualityService]
 })
 export class AppModule {}
---
+++ apps/air-q/src/app/locations/los-angeles/los-angeles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-los-angeles',
+  templateUrl: './los-angeles.component.html',
+  styleUrls: ['./los-angeles.component.css']
+})
+export class LosAngelesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
